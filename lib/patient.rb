d6a7fad8c65c1473ea72e8e39e9459c3d7549060
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-06 15:16:18 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,15 @@
 class Patient
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name=name
   end
+  
+  def self.all
+    @@all
+  end 
   
    def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
