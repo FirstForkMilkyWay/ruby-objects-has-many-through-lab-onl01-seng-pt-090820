72563e1e6d6a39f118d00d0fb1bc886c95ab51d6
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-06 15:45:37 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,8 +5,8 @@ class Appointment
 
   def initialize(date, patient, doctor)
     @date = date
+    @patient = patient 
     @doctor = doctor
-    doctor.add_appointment(self)
 
     @@all << self
   end
