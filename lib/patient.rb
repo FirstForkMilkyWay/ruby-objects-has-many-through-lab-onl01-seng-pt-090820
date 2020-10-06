ddb75d8a43c56cee78e2a7095b590c586bd63c64
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-06 15:14:58 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,33 +1,24 @@
-class Patient 
+class Patient
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name=name
+  end
+  
+   def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
 
-@@all = [ ]
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
 
-attr_accessor :name 
-
-def initialize(name)
-  @name = name 
-  @@all << self 
-end 
-
-def self.all
-  @@all 
-end 
-
-def new_appointment(date, doctor)
-  Appointment.new(date,self,doctor)
-end 
-
-def appointments 
-  Appointment.all.select do |appt|
-    appt.patient == self 
-  end 
-end 
-
-def doctors 
-  appointments.map do |apt|
-    apt.doctors 
-  end 
-end 
-
-
-end 
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
+  
+end
